bfd6b4d6211443f047a0acff8ba830ad0ce33745
Luiz Henrique <luizhenriquecorreia1997@gmail.com>
2021-05-23 21:36:26 -0300

    feat: customer model

---
+++ src/models/customer.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+    businessKey: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    birthday: {
+        type: Date
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    cpf: {
+        type: String
+    },
+    rg: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    address: {
+        zipCode: {
+            type: String
+        },
+        street: {
+            type: String
+        },
+        complement: {
+            type: String
+        },
+        number: {
+            type: Number
+        },
+        neighborhood: {
+            type: String
+        },
+        city: {
+            type: String
+        },
+        state: {
+            type: String
+        },
+        country: {
+            type: String
+        }
+    },
+    about: {
+        type: String
+    }
+});
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+
+module.exports = Customer;
